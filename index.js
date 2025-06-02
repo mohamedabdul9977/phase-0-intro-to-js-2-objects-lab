2dd3b21ca93c7cf2391f9b54443b88891a81ec25
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-06-02 18:01:30 +0300

    Added functions with objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : "Mohamed",
+    streetAddress : "1000",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee};
+    delete newEmployee[key],[value];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key],[value];
+    return employee;
+}
+
+
+
+
